b00e824df9752327e68b10f634a465a86812b3b3
Espoachan <leoponce200111@gmail.com>
2026-02-07 15:24:53 -0600

    Now it calls WideCharToMultiByte only once

--- core/wstring_to_utf8.cpp
+++ core/wstring_to_utf8.cpp
@@ -3,11 +3,12 @@
 std::string wstringToUtf8(const WCHAR* p_file_name, int char_count) {
     if (char_count == 0) return "";
 
-    int buffer_size = WideCharToMultiByte(CP_UTF8, 0, p_file_name, char_count, nullptr, 0, nullptr, nullptr);
-
+    int buffer_size = char_count * 3;
     std::string utf8_string(buffer_size, 0);
 
-    WideCharToMultiByte(CP_UTF8, 0, p_file_name, char_count, &utf8_string[0], buffer_size, nullptr, nullptr);
+    int bytes_written = WideCharToMultiByte(CP_UTF8, 0, p_file_name, char_count, &utf8_string[0], buffer_size, nullptr, nullptr);
+    utf8_string.resize(bytes_written);
 
     return utf8_string;
+
 }
